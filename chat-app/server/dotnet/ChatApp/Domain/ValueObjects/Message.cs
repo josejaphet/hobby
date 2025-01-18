c0cd3f8856e9d7bc6db71122e59a8c62c0ae77f0
Jose Japhet Cipriano <josejaphet.cipriano@outlook.com>
2025-01-18 18:29:13 +0800

    Added Domain value objects

---
+++ chat-app/server/dotnet/ChatApp/Domain/ValueObjects/Message.cs
@@ -0,0 +1,57 @@
+﻿using SecurityDriven;
+
+namespace Domain.ValueObjects;
+public class Message
+{
+    private Message(
+        Guid senderId,
+        Guid receiverId,
+        Guid? groupId,
+        string content,
+        List<string> types,
+        string status)
+    {
+        SenderId = senderId;
+        ReceiverId = receiverId;
+        GroupId = groupId;
+        Content = content;
+        Type = types;
+        Status = status;
+    }
+    public Guid Id { get; private set; }
+
+    public Guid SenderId { get; private set; }
+
+    public Guid ReceiverId { get; private set; }
+
+    public Guid? GroupId { get; private set; }
+
+    public string Content { get; private set; }
+
+    public List<string> Type { get; private set; }
+
+    public string Status { get; private set; }
+
+    public DateTime CreatedOn { get; private set; }
+
+    public static Message Create(
+        Guid senderId,
+        Guid receiverId,
+        Guid? groupId,
+        string content,
+        List<string> types,
+        string status)
+    {
+        var message = new Message(
+                            senderId,
+                            receiverId,
+                            groupId,
+                            content,
+                            types,
+                            status)
+        {
+            Id = FastGuid.NewGuid()
+        };
+        return message;
+    }
+}
---
+++ chat-app/server/dotnet/ChatApp/Domain/ValueObjects/User.cs
@@ -0,0 +1,87 @@
+﻿using SecurityDriven;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.ValueObjects;
+public sealed class User
+{
+    public User(
+        string firstname,
+        string? middlename,
+        string lastname,
+        DateOnly dateOfBirth,
+        string mobileNumber,
+        string passwordHash,
+        string status,
+        string username,
+        string emailAddress
+        )
+    {
+        Firstname = firstname;
+        Middlename = middlename;
+        Lastname = lastname;
+        DateOfBirth = dateOfBirth;
+        MobileNumber = mobileNumber;
+        PasswordHash = passwordHash;
+        Status = status;
+        Username = username;
+        EmailAddress = emailAddress;
+
+    }
+    public Guid Id { get; set; }
+
+    public string Firstname { get; set; }
+
+    public string? Middlename { get; set; }
+
+    public string Lastname { get; set; }
+
+    public DateOnly DateOfBirth { get; set; }
+
+    public string MobileNumber { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? UpdatedOn { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public string PasswordHash { get; set; }
+
+    public string Status { get; set; }
+
+    public string Username { get; set; }
+
+    public string EmailAddress { get; set; }
+
+    public static User Create(
+        string firstname,
+        string? middlename,
+        string lastname,
+        DateOnly dateOfBirth,
+        string mobileNumber,
+        string passwordHash,
+        string status,
+        string username,
+        string emailAddress
+        )
+    {
+        var user = new User(
+                        firstname,
+                        middlename,
+                        lastname, 
+                        dateOfBirth, 
+                        mobileNumber, 
+                        passwordHash,
+                        status,
+                        username,
+                        emailAddress)
+        {
+            Id = FastGuid.NewGuid()
+        };
+        return user;
+    }
+}
